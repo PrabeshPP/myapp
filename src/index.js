b7b9ca11e3b6d5f5e854deddc70427131a1f67aa
Prabesh <prabesh.bista369@gmail.com>
2022-01-15 19:18:13 +0545

    exploring reacts JS

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import ReactDOM from "react-dom";
 import React  from "react";
 const page=(
-   <nav>
-         <h1>Prabesh</h1>
-         <ul>
-             <li>
-                 Pricing
-             </li>
-             <li>
-                 About
-             </li>
-             <li>
-                 Contact
-             </li>
-         </ul>
-
-     </nav>
+  <div>
+      <img src="./logo512.png" width={"40px"} alt="react logo"></img>
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li> Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K starts on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
    
 )
 
