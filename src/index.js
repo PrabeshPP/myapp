8a3737408aa488f64e93792ad78c6c51c8dc65d8
Prabesh <prabesh.bista369@gmail.com>
2022-01-15 18:22:08 +0545

    exploring react js for frontend blockchain tech

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import react from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+const page=(
+   <nav>
+         <h1>Prabesh</h1>
+         <ul>
+             <li>
+                 Pricing
+             </li>
+             <li>
+                 About
+             </li>
+             <li>
+                 Contact
+             </li>
+         </ul>
+
+     </nav>
+   
+)
+
+ReactDOM.render(
+   page
+   ,
+    document.getElementById('root')
+)
