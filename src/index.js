32df9a30ec156744bb5d29f9352b76f1fa939fce
Prabesh <prabesh.bista369@gmail.com>
2022-01-15 21:22:30 +0545

    styling the page

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
 import React from "react";
 import reactDom  from "react-dom";
+import "./index.css"
 
 function Header(){
-return(
+return(<div className="main">
         <header>
-            <nav>
-                <img src="./logo512.png" width={"40px"} alt="react logo"/>
+            <nav className="nav">
+                <img src="./logo512.png" alt="react logo"/>
+                <ul className="navBar" >
+                    <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"none"}>Pricing</a></li>
+                    <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"noone"}>About</a></li>
+                    <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"none"}>Contact</a></li>
+                </ul>
             </nav>
-        </header>);
+        </header>
+        </div>);
 }
 
 
 function MainComponent(){
     return(
-        <>
+        <div>
         <h1>Reasons I am excited to learn React.</h1>
         <ol>
             <li>I want to use web tech for blockchain technology.</li>
             <li>nothing</li>
             <li>nothing1</li>
         </ol>
-        </>
+        </div>
     )
 }
 
 function Footer(){
     return(
-    <footer>@ Bista development.All right reserved.</footer>
+    <div className="footer">
+        <footer>@ Bista development.All right reserved.</footer>
+    </div>
     )
 }
 
 function CustomPage(){
     return(
-        <div>
+        <div >
     <Header/>
     <MainComponent/>
     <Footer/>
