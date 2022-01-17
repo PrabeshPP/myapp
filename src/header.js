b834a0b464f0905499c7643a7833b7d07ec32e3c
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 19:05:53 +0545

    project 1 completed

--- src/header.js
+++ src/header.js
@@ -2,15 +2,14 @@ import React from "react";
 
 // Header Component
 export default function Header(){
-    return(<div className="main">
-            <header>
-                <nav className="nav">
-                   <div className="div1"><img src="./logo512.png" alt="react logo"/>
-                  <h3>ReactFacts</h3></div> 
+    return(
+                <nav >
+                   <img src="./logo512.png" alt="react logo"/>
+                  <h3>ReactFacts</h3>
                   <h4>React Course -Project 1</h4>
                 </nav>
-            </header>
-            </div>);
-    }
+            
+    )
+}
 
 // export default Header
--- src/mainComponent.js
+++ src/mainComponent.js
@@ -3,14 +3,16 @@ import React from "react"
 
 export default function MainComponent(){
     return(
-        <div className="mainC">
-        <h1>Facts about React</h1>
-        <ol>
-            <li>I want to use web tech for blockchain technology.</li>
-            <li>nothing</li>
-            <li>nothing1</li>
-        </ol>
-        </div>
+        <main>
+        <h1 className="main--title">Fun Facts About React</h1>
+        <ul className="main--facts">
+            <li>Was first released in 2013</li>
+            <li>Was Originally Created by Jordan Walke</li>
+            <li> Has well over 100k stars on GitHub</li>
+            <li> Is maintained by Facebook</li>
+            <li> Powers thousands of enterprise apps,including mobile apps</li>
+        </ul>
+        </main>
     )
 }
 
