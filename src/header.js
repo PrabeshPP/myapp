f0091253d79f1cd9460ba86d807e2b74766aa106
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 17:32:09 +0545

    header file ----->>>>

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+    return(<div className="main">
+            <header>
+                <nav className="nav">
+                    <img src="./logo512.png" alt="react logo"/>
+                    <ul className="navBar" >
+                        <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"none"}>Pricing</a></li>
+                        <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"noone"}>About</a></li>
+                        <li><a href="https://github.com/PrabeshPP/Website/blob/master/CSS/style.css" target={"none"}>Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+            </div>);
+    }
