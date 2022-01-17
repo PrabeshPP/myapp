6da0b0d2ec526cac96665136fb3bd5b7486b828e
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 17:38:09 +0545

    mainComponent

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import React from "react";
 import reactDom  from "react-dom";
 import "./index.css"
 import Header from "./header";
+import MainComponent from "./mainComponent";
 
 
-
-function MainComponent(){
-    return(
-        <div>
-        <h1>Reasons I am excited to learn React.</h1>
-        <ol>
-            <li>I want to use web tech for blockchain technology.</li>
-            <li>nothing</li>
-            <li>nothing1</li>
-        </ol>
-        </div>
-    )
-}
 
 function Footer(){
     return(
---
+++ src/mainComponent.js
@@ -0,0 +1,16 @@
+
+import React from "react"
+
+export default function MainComponent(){
+    return(
+        <div>
+        <h1>Reasons I am excited to learn React.</h1>
+        <ol>
+            <li>I want to use web tech for blockchain technology.</li>
+            <li>nothing</li>
+            <li>nothing1</li>
+        </ol>
+        </div>
+    )
+}
+
